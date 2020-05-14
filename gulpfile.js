45d834971cac58ba68e0f5a7b3f2fb501461b297
Jacob-Friesen <jacob@jacobfriesen.com>
2020-05-14 16:28:18 -0400

    ZWEB-3674 Fixed gulp security issues and related packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,73 +3,59 @@
 //////////////////////////////////////////////
 
 // the main gulp reference
-var gulp = require('gulp');
+const gulp = require('gulp');
 
 // deletes files used during build (https://www.npmjs.com/package/gulp-clean)
-var clean = require('gulp-clean');
+const clean = require('gulp-clean');
 
 // combines files into a single destination file (https://github.com/wearefractal/gulp-concat)
-var concat = require('gulp-concat');
+const concat = require('gulp-concat');
 
 // angular.js annotation for compression (https://www.npmjs.com/package/gulp-ng-annotate)
-var annotate = require('gulp-ng-annotate');
+const annotate = require('gulp-ng-annotate');
 
 // minification and variable/parameter renaming (https://www.npmjs.com/package/gulp-uglify)
-var uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify');
 
 // add an IIFE to each file () 
-var iife = require("gulp-iife");
+const iife = require('gulp-iife');
 
 // just prints a filesize of a file (https://www.npmjs.com/package/gulp-filesize)
-var filesize = require('gulp-filesize');
-
-// watches files for changes and reruns tasks (https://www.npmjs.com/package/gulp-watch)
-var watch = require('gulp-watch');
+const filesize = require('gulp-filesize');
 
 // remove debug statements in the app code (https://www.npmjs.com/package/gulp-strip-debug)
-var stripDebug = require('gulp-strip-debug');
+const stripDebug = require('gulp-strip-debug');
 
 // karma server to run automated unit tests (http://karma-runner.github.io/0.13/index.html)
-var Server = require('karma').Server;
-
-// jscs JS Code Style checker.  (http://jscs.info)
-var jscs = require('gulp-jscs');
+const Server = require('karma').Server;
 
 // sourcemaps (https://www.npmjs.com/package/gulp-sourcemaps)
-var sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps');
 
 // gulp-bump (https://www.npmjs.com/package/gulp-bump)
-var bump = require('gulp-bump');
+const bump = require('gulp-bump');
 
 // git-describe (https://www.npmjs.com/package/git-describe)
-var gitDescribe = require('git-describe');
+const { gitDescribeSync } = require('git-describe');
 
 // used for renaming the css style output to "build.css"
-var rename = require('gulp-rename');
-
-// used for generating font files from svg icons
-var iconfont = require('gulp-iconfont');
-
-// used by gulp-iconfont
-var consolidate = require('gulp-consolidate');
+const rename = require('gulp-rename');
 
 //////////////////////////////////////////////
 // Variables
 //////////////////////////////////////////////
 
 // All application JS files.
-var appFiles = [
-    //'api/models/**/*.model.js',
-    'src/**/*.js'];
-
+const appFiles = [
+    'src/**/*.js'
+];
 
 //////////////////////////////////////////////
 // Tasks
 //////////////////////////////////////////////
 
 // wrap all angular code in bracket and add useStrict, and add sourcemap in dev
-gulp.task('lib', function () {
-
+gulp.task('lib', () => {
     return gulp.src(appFiles)
         .pipe(iife({
             useStrict: true,
@@ -82,62 +68,48 @@ gulp.task('lib', function () {
         .pipe(gulp.dest('dist/'));
 });
 
-
-
 // single run testing
-gulp.task('test', function (done) {
-    new Server({ configFile: __dirname + '/karma.conf.js', singleRun: true },
-        function (code) {
-            if (code == 1) {
-                console.log('Unit Test failures, exiting process');
-                //done(new Error(`Karma exited with status code ${code}`));
-                return process.exit(code);
-            } else {
-                console.log('Unit Tests passed');
-                done();
-            }
-        }).start();
+gulp.task('test', (done) => {
+    new Server({ configFile: __dirname + '/karma.conf.js', singleRun: true }, (code) => {
+        if (code == 1) {
+            console.log('Unit Test failures, exiting process');
+            //done(new Error(`Karma exited with status code ${code}`));
+            return process.exit(code);
+        } else {
+            console.log('Unit Tests passed');
+            done();
+        }
+    }).start();
 });
 
 // continuous testing
-gulp.task('tdd', function (done) {
-    new Server({ configFile: __dirname + '/karma.conf.js' }, function () {
+gulp.task('tdd', (done) => {
+    new Server({ configFile: __dirname + '/karma.conf.js' }, () => {
         done();
     }).start();
 });
 
-// watch the app .js files for changes and execute the app-js task if necessary
-gulp.task('app-watch', function () {
-    watch(appFiles, function (file) {
-        //gulp.start('app-js-dev');
-    });
-});
-
 // clean up files after builds
-gulp.task('cleanup', function () {
-    return gulp.src('build', { read: false })
+gulp.task('cleanup', () => {
+    return gulp.src('dist', { read: false })
         .pipe(clean());
 });
 
 // bump the dev version (NOTE: NOT IN USE RIGHT NOW)
-gulp.task('bump-dev', function () {
-    var gitInfo = gitDescribe(__dirname);
+gulp.task('bump-dev', () => {
+    const gitInfo = gitDescribeSync(__dirname);
 
-    gulp.src(['./bower.json', './package.json'])
+    return gulp.src(['./package.json'])
         .pipe(bump({ type: 'prerelease', preid: gitInfo.hash }))
         .pipe(gulp.dest('./'));
 });
 
 // build angular-socketio.js for dev (with map) and prod (min)
-gulp.task('build', ['lib'], function () {
-    gulp.start(['test', 'cleanup']);
-});
-
+gulp.task('build', gulp.series('lib', () => {
+    return gulp.series('test', 'cleanup')();
+}));
 
 // continuous watchers
-gulp.task('default', ['lib'], function () {
-    gulp.start(['app-watch', 'tdd']);
-});
-
-
-
+gulp.task('default', gulp.series('lib', () => {
+    return gulp.series('tdd')();
+}));
